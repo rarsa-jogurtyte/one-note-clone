f640a287fb33a75e19f53b43fec254ed60df0057
Rasa Jurgutytė <rjurgutyte@alna.lt>
2021-10-15 13:06:19 +0300

    select color scheme and icons

--- src/Home/Navbar.tsx
+++ src/Home/Navbar.tsx
@@ -1,28 +1,23 @@
-import React from "react";
+import "../Styles/Navbar.css";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export default function NavBar() {
     return (
         <>
-            <Navbar>
+            <Navbar className="navbar">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">
+                        <i className="fas fa-pencil-alt"></i> UnoNote
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
+                            <Nav.Link className="nav-item" href="#home">
+                                Home
+                            </Nav.Link>
+                            <Nav.Link className="nav-item" href="#link">
+                                Link
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
